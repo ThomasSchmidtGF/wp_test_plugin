b9bce851085e01b84dde3c4e3acabd1e5b47b0a9
Thomas Schmidt ( Work) <thomas.schmidt@gastfreund.net>
2018-06-19 16:11:53 +0200

    fixed another crazy bug in the code. Copy Pasta is not the best meal...

--- admin/class-wp_test_plugin-admin.php
+++ admin/class-wp_test_plugin-admin.php
@@ -72,71 +72,8 @@ class Wp_test_plugin_Admin {
 		 * between the defined hooks and the functions defined in this
 		 * class.
 		 */
-
 		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/wp_test_plugin-admin.css', array(), $this->version, 'all' );
-		if ( 'settings_page_ $(function(){
-
-			// Let's set up some variables for the image upload and removing the image     
-			var frame,
-				imgUploadButton = $( '#upload_login_logo_button' ),    
-				imgContainer = $( '#upload_logo_preview' ),
-				imgIdInput = $( '#login_logo_id' ),
-				imgPreview = $('#upload_logo_preview'),        
-				imgDelButton = $('#wp_cbf-delete_logo_button'),
-				// Color Pickers Inputs
-				colorPickerInputs = $( '.wp-cbf-color-picker' );
-   
-			// WordPress specific plugins - color picker and image upload
-			$( '.wp_test_plugin-color-picker' ).wpColorPicker();
-   
-		   // wp.media add Image
-			imgUploadButton.on( 'click', function( event ){
-   
-			   event.preventDefault();
-				//new comment, i need testmaterial
-			   // If the media frame already exists, reopen it.
-			   if ( frame ) {
-				 frame.open();
-				 return;
-			   }
-   
-			   // Create a new media frame
-			   frame = wp.media({
-				 title: 'Select or Upload Media for your Login Logo',
-				 button: {
-				   text: 'Use as my Login page Logo'
-				 },
-				 multiple: false  // Set to true to allow multiple files to be selected
-			   });
-			   // When an image is selected in the media frame...
-			   frame.on( 'select', function() {
-   
-				 // Get media attachment details from the frame state
-				 var attachment = frame.state().get('selection').first().toJSON();                
-   
-				 // Send the attachment URL to our custom image input field.
-				 imgPreview.find( 'img' ).attr( 'src', attachment.sizes.thumbnail.url );
-   
-				 // Send the attachment id to our hidden input
-				 imgIdInput.val( attachment.id );
-   
-				 // Unhide the remove image link
-				 imgPreview.removeClass( 'hidden' );
-			   });
-   
-			   // Finally, open the modal on click
-			   frame.open();
-		   });
-   
-		   // Erase image url and age preview
-		   imgDelButton.on('click', function(e){
-			   e.preventDefault();
-			   imgIdInput.val('');
-			   imgPreview.find( 'img' ).attr( 'src', '' );
-			   imgPreview.addClass('hidden');
-		   });
-   
-	   }); // End of DOM Ready' == get_current_screen() -> id ) {
+		if ( 'settings_page_wp-cbf' == get_current_screen() -> id ) {
 			// CSS stylesheet for Color Picker
 			wp_enqueue_style( 'wp-color-picker' );            
 			wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/wp_test_plugin-admin.css', array( 'wp-color-picker' ), $this->version, 'all' );
