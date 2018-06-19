3617aaa85a91c624bd1dd576d1a10f38300a052f
Thomas Schmidt ( Work) <thomas.schmidt@gastfreund.net>
2018-06-19 16:24:00 +0200

    more renaming and bugfixing. Copy pasta is no fun life

--- admin/partials/wp_test_plugin-admin-display.php
+++ admin/partials/wp_test_plugin-admin-display.php
@@ -135,14 +135,14 @@
                     <input id="upload_login_logo_button" type="button" class="button" value="<?php _e( 'Upload Logo', $this->plugin_name); ?>" />
                     <span><?php esc_attr_e('Login Logo', $this->plugin_name);?></span>
                 </label>
-                <div id="upload_logo_preview" class="wp_cbf-upload-preview <?php if(empty($login_logo_id)) echo 'hidden'?>">
+                <div id="upload_logo_preview" class="wp_test_plugin-upload-preview <?php if(empty($login_logo_id)) echo 'hidden'?>">
                     <img src="<?php echo $login_logo_url; ?>" />
-                    <button id="wp_cbf-delete_logo_button" class="wp_cbf-delete-image">X</button>
+                    <button id="wp_test_plugin-delete_logo_button" class="wp_test_plugin-delete-image">X</button>
                 </div>
             </fieldset>
 
         <!-- login background color-->
-            <fieldset class="wp_cbf-admin-colors">
+            <fieldset class="wp_test_plugin-admin-colors">
                 <legend class="screen-reader-text"><span><?php _e('Login Background Color', $this->plugin_name);?></span></legend>
                 <label for="<?php echo $this->plugin_name;?>-login_background_color">
                     <input type="text" class="<?php echo $this->plugin_name;?>-color-picker" id="<?php echo $this->plugin_name;?>-login_background_color" name="<?php echo $this->plugin_name;?>[login_background_color]" value="<?php echo $login_background_color;?>" />
@@ -151,7 +151,7 @@
             </fieldset>
 
         <!-- login buttons and links primary color-->
-            <fieldset class="wp_cbf-admin-colors">
+            <fieldset class="wp_test_plugin-admin-colors">
                 <legend class="screen-reader-text"><span><?php _e('Login Button and Links Color', $this->plugin_name);?></span></legend>
                 <label for="<?php echo $this->plugin_name;?>-login_button_primary_color">
                     <input type="text" class="<?php echo $this->plugin_name;?>-color-picker" id="<?php echo $this->plugin_name;?>-login_button_primary_color" name="<?php echo $this->plugin_name;?>[login_button_primary_color]" value="<?php echo $login_button_primary_color;?>" />
