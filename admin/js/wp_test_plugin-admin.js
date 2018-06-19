1fef784e5fc274bf5a85fc84e760f7e65c70f265
Thomas Schmidt ( Work) <thomas.schmidt@gastfreund.net>
2018-06-19 16:25:17 +0200

    buuuuuuuuuuuuuuugfixing

--- admin/js/wp_test_plugin-admin.js
+++ admin/js/wp_test_plugin-admin.js
@@ -37,7 +37,7 @@
 			imgContainer = $( '#upload_logo_preview' ),
 			imgIdInput = $( '#login_logo_id' ),
 			imgPreview = $('#upload_logo_preview'),        
-			imgDelButton = $('#wp_cbf-delete_logo_button'),
+			imgDelButton = $('#wp_test_plugin-delete_logo_button'),
 			// Color Pickers Inputs
 			colorPickerInputs = $( '.wp_test_plugin-color-picker' );
 
