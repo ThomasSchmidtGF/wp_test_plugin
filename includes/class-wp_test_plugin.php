520e81683ed067f321199e64acd862f8047a826f
Thomas Schmidt ( Work) <thomas.schmidt@gastfreund.net>
2018-06-19 17:30:16 +0200

    adding more functionality

--- includes/class-wp_test_plugin.php
+++ includes/class-wp_test_plugin.php
@@ -163,6 +163,7 @@ class Wp_test_plugin {
 		$plugin_basename = plugin_basename( plugin_dir_path( __DIR__ ) . $this->plugin_name . '.php' );
 		$this->loader->add_filter( 'plugin_action_links_' . $plugin_basename, $plugin_admin, 'add_action_links' );
 		$this->loader->add_action('admin_init', $plugin_admin, 'options_update');
+		$this->loader->add_action( 'login_enqueue_scripts', $plugin_admin, 'wp_test_plugin_login_css' );
 
 	}
 
@@ -188,6 +189,8 @@ class Wp_test_plugin {
 		$this->loader->add_filter( 'body_class', $plugin_public, 'wp_test_plugin_body_class_slug' );
 
 	}
+
+	
 
 	/**
 	 * Run the loader to execute all of the hooks with WordPress.
